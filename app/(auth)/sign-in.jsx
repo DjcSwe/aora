a58a784c890a0b1bc44b9f8983f272cf7922bbcb
Daniel Condly <settle.never@aol.com>
2024-12-05 18:07:17 -0500

    check and delete sessions for user login

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import { View, Text, ScrollView, Image, Alert, ActivityIndicator } from 'react-native'
 import { React, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants"
@@ -9,37 +9,32 @@ import { checkActiveSession, deleteSessions, signIn } from '../../lib/appwrite'
 
 const SignIn = () => {
 
+   const [isSubmitting, setisSubmitting] = useState(false)
    const [form, setForm] = useState({
       email: "",
       password: "",
    })
-
-   const [isSubmitting, setisSubmitting] = useState(false)
-
-   // const submit = async () => {
-   //    validateInput()
-   //    if (validateInput()) {
-   //       setisSubmitting(true);
-   //       // Loading indicator
-   //       if (checkActiveSession()) await deleteSessions()
-   //       await signIn(form.email, form.password); 
-   //       // TODO: set form data to global state
-   //       router.replace("/home");
-   //    }
-   // };
-
-   const logIn = async () => {
-      if (checkActiveSession()) 
-         await deleteSessions()
-      await signIn(form.email, form.password); 
-      router.replace("/home");
-   }
 
    function validateInput() {
       const emailRegex =/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!emailRegex.test(form.email)) Alert.alert("Error", "Please enter a valid email.");
       else if (!form.password) Alert.alert("Error", "Please enter a valid password.");
       else logIn()
+   }
+
+   async function logIn() {
+      setisSubmitting(true);
+      try {
+         if (await checkActiveSession()) await deleteSessions();
+         await signIn(form.email, form.password);
+         router.replace("/home");
+      } catch (error) {
+         setisSubmitting(false);
+         // TODO: get unique error messages
+         Alert.alert("Error", "There was an issue singning in. Please try again.");
+         console.log("logIn: ", error);
+         throw error;
+      }
    }
 
    return (
@@ -72,6 +67,14 @@ const SignIn = () => {
                   <Link href="/sign-up" className='text-lg text-secondary font-semibold'>Sign Up</Link>
                </View>
             </View>
+            {isSubmitting ? (
+            <View>
+               <ActivityIndicator size="large" />
+               <Text className="text-center text-lg text-gray-100 font-pregular mt-4">
+                  Signing in...
+               </Text>
+            </View>
+         ) : null}
          </ScrollView>
       </SafeAreaView>
    )
