7a87f934da7bf2f4452b1f6ae18f0fe2d5164a28
Daniel Condly <settle.never@aol.com>
2024-12-05 18:07:32 -0500

    profile logout button

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { checkActiveSession, deleteSessions, signIn } from '../../lib/appwrite'
+import { router } from 'expo-router';
 
 const Profile = () => {
+
+   function logOut() {
+      try {
+         deleteSessions();
+         router.replace("/sign-in")
+      } catch (error) {
+         console.log("logout: ", error)
+         throw error
+      }
+   }
+
    return (
       <View>
          <Text>Profile</Text>
+         <Button title='Log Out' onPress={logOut}/>
       </View>
    )
 }
