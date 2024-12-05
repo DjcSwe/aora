1e8be9788b2a9d476e2c10e48b0af096b2d4ab21
Daniel Condly <settle.never@aol.com>
2024-12-04 23:51:49 -0500

    formatting

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,4 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  Image,
-  Alert,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, Image, Alert, ActivityIndicator } from "react-native";
 import { React, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
