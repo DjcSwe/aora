244e649bf131e5fccb4b930297c5f62cd115122a
Daniel Condly <settle.never@aol.com>
2025-01-02 18:51:52 -0500

    refactor

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
                   <SearchInput />
                   <View className="w-full flex-1 pt-5 pb-8">
                      <Text className="text-gray-100 text-lg font-pregular mb-3 ">
-                        Latest Videos
+                        Trending Videos
                      </Text>
 
                      <Trending posts={latestPosts ?? []} />
