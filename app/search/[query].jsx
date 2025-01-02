0cf26147b6831fff682334f198824859b3dcb739
Daniel Condly <settle.never@aol.com>
2025-01-02 18:50:10 -0500

    search query

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,14 @@
-// Rename page or get the query information for the specific page that the user is trying to do
-
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 const Search = () => {
+   const { query } = useLocalSearchParams();
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="text-3xl text-white">{query}</Text>
+    </SafeAreaView>
   )
 }
 
