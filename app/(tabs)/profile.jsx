10929ef74eec41c66a3066494c922be47a1761c7
Daniel Condly <settle.never@aol.com>
2025-01-03 21:46:52 -0500

    profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,24 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { checkActiveSession, deleteSessions, signIn } from '../../lib/appwrite'
-import { router } from 'expo-router';
+import {router} from 'expo-router';
+import {Button, FlatList, TouchableOpacity, View, Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {deleteSessions, getUserPosts} from '../../lib/appwrite'
+import useAppWrite from "../../lib/useAppwrite";
+import {useGlobalContext} from "../../context/GlobalProvider";
+import EmptyState from "../../components/EmptyState";
+import VideoCard from "../../components/VideoCard";
+import {icons} from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
+   const { user, setUser, setIsLoggedIn } = useGlobalContext();
+   const { data: posts } = useAppWrite(() => getUserPosts(user.$id));
+   console.log("Posts: "+ posts);
+   console.log("user.$id: " + user.$id);
 
    function logOut() {
       try {
-         deleteSessions();
+         deleteSessions().then(r => {console.log(r)});
          router.replace("/sign-in")
       } catch (error) {
          console.log("logout: ", error)
@@ -17,13 +27,58 @@ const Profile = () => {
    }
 
    return (
-      <SafeAreaView>
-         <Text>Profile</Text>
-         <Button title='Log Out' onPress={logOut}/>
+      <SafeAreaView className="bg-primary h-full" edges={['top', 'left', 'right']}>
+         <FlatList
+            data={posts}
+            keyExtractor={(item) => item.$id}
+            renderItem={({ item }) => (
+               <VideoCard video={item}/>
+            )}
+            ListHeaderComponent={() => (
+               <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+                  <TouchableOpacity
+                     className='w-full items-end mb-10'
+                     onPress={() => logOut()}
+                  >
+                     <Image
+                        source={icons.logout}
+                        resizeMode='contain'
+                        className='w-6 h-6'
+                     />
+                  </TouchableOpacity>
+                  <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+                     <Image
+                        source={{uri: user?.avatar }}
+                        className="w-[90%] h-[90%] rounded-lg"
+                        resizeMode='cover'
+                     />
+                  </View>
+                  <InfoBox
+                     title={user?.username}
+                     containerStyles='mt-5'
+                     titleStyles='text-lg'
+                  />
+                  <View className='mt-5 flex-row'>
+                     <InfoBox
+                        title={posts.length || 0}
+                        subtitle="Posts"
+                        containerStyles='mr-10'
+                        titleStyles='text-xl'
+                     />
+                     <InfoBox
+                        title="1.4k"
+                        subtitle="Followers"
+                        titleStyles='text-xl'
+                     />
+                  </View>
+               </View>
+            )}
+            ListEmptyComponent={() => (
+               <EmptyState title="No Videos Found"/>
+            )}
+         />
       </SafeAreaView>
    )
 }
 
 export default Profile
-
-const styles = StyleSheet.create({})
